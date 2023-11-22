7ca80c464838daefccf0e8a0c9fddcdc8b3e6f01
woo0703 <80863905+woo0703@users.noreply.github.com>
2023-11-22 23:41:51 +0900

    2023/11/22 11:41pm project done

--- src/server.js
+++ src/server.js
@@ -4,31 +4,40 @@ const PORT = 4000;
 
 const app = express();
 
+// #1) URL Logger 
+app.use((req, res, next) => {
+    console.log(`Path: ${req.path}`);
+    next();
+});
 
-app.get("/", (req,res) => {
-    return res.send("<h1>home</h1>");
-})
-app.get("/about", (req,res) => {
-    res.send("<h1>About</h1>");
-})
-app.get("/contact", (req,res) => {
-    res.send("<h1>Contact</h1>");
-})
-app.get("/login", (req,res) => {
-    res.send("<h1>Login</h1>");
-})
+// #2) Time Logger 
+app.use((req, res, next) => {
+    const now = new Date();
+    console.log(`Time: ${now.getFullYear()}.${now.getMonth() + 1}.${now.getDate()}`);
+    next();
+});
+
+// #3) Security Logger 
+app.use((req, res, next) => {
+    const protocol = req.protocol;
+    console.log(protocol === 'https' ? 'Secure' : 'Insecure');
+    next();
+});
+
+// #4) Protector 
+app.use('/protected', (req, res, next) => {
+    res.send('Access Denied');
+});
+
+// [ Home route ]
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
 
 
-
-
-
-
-
-
-
-
-
-
-const handleListening = () => console.log("Server listening on port http://localhost:${PORT}")
+        
+const handleListening = () => 
+    console.log("Server listening on port http://localhost:${PORT}")
 
 app.listen(PORT, handleListening);
+        
